cff5e7169b372272cbf5dd5ea3cba337eca01be7
Srun Oudomsambath <toyroith@gmail.com>
2026-02-25 08:38:23 +0700

    feat: add KycEntity with OneToOne relation to CustomerEntity

---
+++ microservices/customer-service/src/main/java/co/istad/sambath/customer_service/data/entity/KycEntity.java
@@ -0,0 +1,29 @@
+package co.istad.sambath.customer_service.data.entity;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "kyc")
+public class KycEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID kycId;
+    private String type;
+    private String number;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "customer_id")
+    private CustomerEntity customer;
+
+
+}
